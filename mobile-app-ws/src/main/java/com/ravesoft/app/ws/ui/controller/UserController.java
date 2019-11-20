e006872cc2141d2112f10baaa74ee4fe95624f28
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-20 16:58:17 +0800

    add path variable @PathVariable

--- mobile-app-ws/src/main/java/com/ravesoft/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/ravesoft/app/ws/ui/controller/UserController.java
@@ -2,9 +2,11 @@ package com.ravesoft.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	
-	@GetMapping
-	public String getUSer() {
-		return "Get user";
+
+	
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "Get user" +userId;
 	}
 	
 	@PostMapping
