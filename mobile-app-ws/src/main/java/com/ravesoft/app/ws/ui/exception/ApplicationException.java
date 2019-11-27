d511aaf23b20d48e5d9d19acd7eb256389d8d4d1
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-27 15:41:51 +0800

    Add custom error message for exception handling

--- mobile-app-ws/src/main/java/com/ravesoft/app/ws/ui/exception/ApplicationException.java
+++ mobile-app-ws/src/main/java/com/ravesoft/app/ws/ui/exception/ApplicationException.java
@@ -1,5 +1,7 @@
 package com.ravesoft.app.ws.ui.exception;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +10,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.ravesoft.app.ws.ui.model.response.ErrorMessage;
+
 
 @ControllerAdvice
 public class ApplicationException extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(value=Exception.class)
 	public ResponseEntity<Object> handleAny(Exception ex, WebRequest request) {
-		return new ResponseEntity<>(ex	, new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		String errorMessageDescription = ex.getLocalizedMessage();
+		
+		if(errorMessageDescription==null)errorMessageDescription=ex.toString();
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(),errorMessageDescription);
+		
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
