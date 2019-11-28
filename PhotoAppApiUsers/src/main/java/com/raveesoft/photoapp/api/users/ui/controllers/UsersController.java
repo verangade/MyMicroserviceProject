7aefd694a1e2162328b25af9a3e0b75c51599391
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-28 22:50:24 +0800

    simulate how load balnce works

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.raveesoft.photoapp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status/check")
 	public String status() {
-		return "Users microservice is working";
+		return "Users microservice is working port : "+env.getProperty("local.server.port");
 	}
 
 }
