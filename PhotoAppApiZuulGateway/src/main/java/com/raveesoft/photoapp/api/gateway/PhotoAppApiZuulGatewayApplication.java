bda1b8ff8398fe57767cea5c7426c92254b36989
Malliyawadu Raveera Veranga De Silva  (NCS) <rv.malliyawadu@ncs.com.sg>
2019-11-28 17:27:22 +0800

    Initialize Zuul API Gateway

---
+++ PhotoAppApiZuulGateway/src/main/java/com/raveesoft/photoapp/api/gateway/PhotoAppApiZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.raveesoft.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiZuulGatewayApplication.class, args);
+	}
+
+}
