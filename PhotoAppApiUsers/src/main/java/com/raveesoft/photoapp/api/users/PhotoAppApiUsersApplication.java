9f5c40ad7b7377d69c561c439a498ec1530395f9
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-28 15:28:25 +0800

    Enable as Eureka client

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,8 +2,10 @@ package com.raveesoft.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
