8c54b9bb7d926946ae80ef46d192da0b37e73a63
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-28 23:24:39 +0800

    added createUser method

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
@@ -3,8 +3,12 @@ package com.raveesoft.photoapp.api.users.ui.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raveesoft.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +20,11 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Users microservice is working port : "+env.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String creaeUser(@RequestBody CreateUserRequestModel userDetails) {
+		return "";
 	}
 
 }
---
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/model/CreateUserRequestModel.java
@@ -0,0 +1,48 @@
+package com.raveesoft.photoapp.api.users.ui.model;
+
+public class CreateUserRequestModel {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	
+	public CreateUserRequestModel() {
+	}
+	
+	public CreateUserRequestModel(String firstName, String lastName, String password, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
