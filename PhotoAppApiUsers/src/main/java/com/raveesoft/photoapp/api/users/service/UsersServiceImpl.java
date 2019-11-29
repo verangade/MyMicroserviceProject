eb3e91a20913d2ca0cbf52173b8085ef6828d671
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-29 15:57:26 +0800

    add password encoding

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.raveesoft.photoapp.api.users.data.UserEntity;
@@ -16,21 +17,25 @@ public class UsersServiceImpl implements UsersService {
 	
 	private UsersRepository usersRepository;
 	
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	
 	@Autowired
-	public UsersServiceImpl(UsersRepository usersRepository) {
+	public UsersServiceImpl(UsersRepository usersRepository,BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
 		userDto.setUserId(UUID.randomUUID().toString());
+		userDto.setEmncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
 		
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity entity = mapper.map(userDto, UserEntity.class);
-		entity.setEmncryptedPassword("test");
+
 		usersRepository.save(entity);
 		
 		UserDto returnValue = mapper.map(entity, UserDto.class); 
