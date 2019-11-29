96bf3994920437a1a8c92b70378608681315d687
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-29 11:25:13 +0800

    Add random alphanumeric number generation s userId

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,12 +1,14 @@
 package com.raveesoft.photoapp.api.users.service;
 
+import java.util.UUID;
+
 import com.raveesoft.photoapp.api.users.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
-		// TODO Auto-generated method stub
+		userDto.setUserId(UUID.randomUUID().toString());
 		return null;
 	}
 
