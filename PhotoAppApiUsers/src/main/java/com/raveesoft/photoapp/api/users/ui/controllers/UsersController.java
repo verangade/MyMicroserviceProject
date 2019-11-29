55ca4f98fd59ca4b927c2086f7be47aa31e0ec07
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-29 14:45:06 +0800

    added http status as 201 (created) more convinient

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
@@ -2,10 +2,13 @@ package com.raveesoft.photoapp.api.users.ui.controllers;
 
 import javax.validation.Valid;
 
+import org.apache.commons.lang.ObjectUtils.Null;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,12 +35,12 @@ public class UsersController {
 	}
 	
 	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+	public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserDto userDto = mapper.map(userDetails, UserDto.class);
 		usersService.createUser(userDto);
-		return "Create user method is called";
+		return new ResponseEntity(HttpStatus.CREATED);
 		
 	}
 
