c5ee9940c7a5b352d45e801435c517b8796097bb
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-29 17:46:36 +0800

    added functionality to accepts requets only from certain ip- In this case zuul api gateway.commented it coz not sure what ip to put
    http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"));

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.raveesoft.photoapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +11,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
+	
+	private Environment env;
+	
+	@Autowired
+	public WebSecurity(Environment env) {
+		this.env = env;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		//http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"));
 		http.headers().frameOptions().disable();
 	}
 	
