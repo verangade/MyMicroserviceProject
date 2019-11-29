f5f3a19769cc5d5eedc003eb2bed6a07cffa7adb
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-29 17:28:39 +0800

    added to produce payload as xmlor json requested

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
@@ -8,6 +8,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +36,8 @@ public class UsersController {
 		return "Users microservice is working port : "+env.getProperty("local.server.port");
 	}
 	
-	@PostMapping
+	@PostMapping(consumes= {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE}
+	,produces= {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
