f7b9620e85f17a68e61988acd6b79ab54503e8bf
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-29 14:34:07 +0800

    added create user functionality

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/service/UsersServiceImpl.java
@@ -2,13 +2,36 @@ package com.raveesoft.photoapp.api.users.service;
 
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raveesoft.photoapp.api.users.data.UserEntity;
+import com.raveesoft.photoapp.api.users.repository.UsersRepository;
 import com.raveesoft.photoapp.api.users.shared.UserDto;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+	
+	private UsersRepository usersRepository;
+	
+	
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository) {
+		this.usersRepository = usersRepository;
+	}
+
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
 		userDto.setUserId(UUID.randomUUID().toString());
+		
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserEntity entity = mapper.map(userDto, UserEntity.class);
+		entity.setEmncryptedPassword("test");
+		usersRepository.save(entity);
 		return null;
 	}
 
--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/ui/controllers/UsersController.java
@@ -2,6 +2,8 @@ package com.raveesoft.photoapp.api.users.ui.controllers;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.raveesoft.photoapp.api.users.service.UsersService;
+import com.raveesoft.photoapp.api.users.shared.UserDto;
 import com.raveesoft.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+	
+	@Autowired
+	private UsersService usersService;
 	
 	@Autowired
 	private Environment env;
@@ -25,8 +32,13 @@ public class UsersController {
 	}
 	
 	@PostMapping
-	public String creaeUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
-		return "";
+	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserDto userDto = mapper.map(userDetails, UserDto.class);
+		usersService.createUser(userDto);
+		return "Create user method is called";
+		
 	}
 
 }
