7b0b159ddedd50aac555c57546306c565022228a
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-11-30 19:36:46 +0800

    add spring security to zuul api gateway. allow certain requets to access without having authorization token in the header like user registration user login h2-console

--- PhotoAppApiZuulGateway/src/main/java/com/raveesoft/photoapp/api/gateway/security/WebSecurity.java
+++ PhotoAppApiZuulGateway/src/main/java/com/raveesoft/photoapp/api/gateway/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.raveesoft.photoapp.api.gateway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -39,11 +40,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 			so that makes our http requests stateless . STATELESS is the most strict policy
 			so most http headers needed to be reauthorize.
 			some requests no need to have authorization header eg. sign up
-			
-			
-			
-		
+	
 		*/
+		
+		//allow requets registration,login no need authorization header for those. other every urls needed
+		http.authorizeRequests().antMatchers("users-ws/login").permitAll()
+		.antMatchers(HttpMethod.POST,env.getProperty("api.registration.url.path")).permitAll()
+		.antMatchers(HttpMethod.POST,env.getProperty("api.login.path.url")).permitAll()
+		.antMatchers(env.getProperty("api.h2-console.url.path")).permitAll()
+		.anyRequest().authenticated(); // other every urls needed authorization header - jwt token
+		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		
