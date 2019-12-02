11b32c9bcae9b9c4704f64334cec98e7163c5e64
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-12-02 23:03:04 +0800

    Temporary disable gateway ip inorder to test properly

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/security/WebSecurity.java
@@ -31,7 +31,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		//http.authorizeRequests().antMatchers("/users/**").permitAll()
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"))
+		http.authorizeRequests().antMatchers("/**").permitAll()
+		//hasIpAddress(env.getProperty("gateway.ip"))
 		.and().addFilter(getAuthenticationFilter());
 		
 		//enable h2-console access
