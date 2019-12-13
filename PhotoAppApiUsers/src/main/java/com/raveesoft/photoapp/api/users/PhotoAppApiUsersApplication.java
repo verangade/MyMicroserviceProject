d8668122f205638fe7b64affaa49f303113c5bc9
verangade <verangades@gmail.com>
2019-12-13 23:25:47 +0800

    Add hystrix breaker @EnableCircuitBreaker
    @FeignClient(name="albums-ws",fallback=AlbumFallback.class)
    public interface AlbumServiceClient {

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,6 +2,7 @@ package com.raveesoft.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -18,6 +19,7 @@ import feign.Logger;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/data/AlbumServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/data/AlbumServiceClient.java
@@ -1,17 +1,30 @@
 package com.raveesoft.photoapp.api.users.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.raveesoft.photoapp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name="albums-ws",fallback=AlbumFallback.class)
 public interface AlbumServiceClient {
 	
 	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 
+}
+
+@Component
+class AlbumFallback implements AlbumServiceClient{
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String id) {
+		
+		return new ArrayList<AlbumResponseModel>();
+	}
+	
 }
