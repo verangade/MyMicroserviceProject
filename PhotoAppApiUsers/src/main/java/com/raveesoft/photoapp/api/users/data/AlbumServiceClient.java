0115b779d6f12c7b9bf46decec1736bb1abe3c60
verangade <verangades@gmail.com>
2019-12-14 12:59:23 +0800

    Error Handling With Feign Hystrix Fallback Factory. while doing fallback we can log the errors if we use this way

--- PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/data/AlbumServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/raveesoft/photoapp/api/users/data/AlbumServiceClient.java
@@ -3,6 +3,8 @@ package com.raveesoft.photoapp.api.users.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.raveesoft.photoapp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name="albums-ws",fallback=AlbumFallback.class)
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name="albums-ws",fallbackFactory=AlbumFallbackFactory.class)
 public interface AlbumServiceClient {
 	
 	@GetMapping("/users/{id}/albums")
@@ -19,12 +24,38 @@ public interface AlbumServiceClient {
 }
 
 @Component
-class AlbumFallback implements AlbumServiceClient{
+class AlbumFallbackFactory implements FallbackFactory<AlbumServiceClient>{
+
+
+	@Override
+	public AlbumServiceClient create(Throwable cause) {
+		return new AlbumServiceClientFallBack(cause);
+	}
+	
+}
+
+
+class AlbumServiceClientFallBack implements AlbumServiceClient{
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private final Throwable cause;
+	
+	public AlbumServiceClientFallBack(Throwable cause) {
+		this.cause=cause;
+	}
 
 	@Override
 	public List<AlbumResponseModel> getAlbums(String id) {
 		
-		return new ArrayList<AlbumResponseModel>();
+		if(cause instanceof FeignException && ((FeignException)cause).status()==404) {
+			logger.error("404 took place when getalbums was with userId "+id + "Error MEssage "+cause.getLocalizedMessage());
+		}else {
+			logger.error("Other error took place " +cause.getLocalizedMessage());
+		}
+		
+		
+		return new ArrayList<>();
 	}
 	
 }
